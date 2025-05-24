0fef65920e328daaf493631a41f8bac588e30b51
Vovalketer <kevinaa021@gmail.com>
2025-05-24 02:16:29 -0300

    refactor: remove inc folder and its contents, use include instead

--- inc/ui.h
+++
@@ -1,9 +0,0 @@
-#ifndef UI_H
-#define UI_H
-
-#include "engine.h"
-
-void init_rendering(int screen_width, int screen_height);
-void game_loop(MatchState *state);
-void stop_rendering(void);
-#endif

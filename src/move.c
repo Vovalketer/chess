c03307b7382e64ff1b5bed4cd22fb4b9b551e8c6
Vovalketer <kevinaa021@gmail.com>
2025-05-24 04:26:21 -0300

    refactor: change MoveList struct to use flexible arr member, ensuring we get a single block of memory

--- src/move.c
+++ src/move.c
@@ -10,17 +10,11 @@ Move move_create(int x_src, int y_src, int x_dest, int y_dest) {
 
 bool move_list_create(MoveList **list) {
 	assert(list != NULL);
-	unsigned int initial_capacity = 8;
-	MoveList *moves = malloc(sizeof(MoveList));
+	static size_t initial_capacity = 8;
+	MoveList *moves = malloc(sizeof(MoveList) + sizeof(Move) * initial_capacity);
 	if (moves == NULL) {
 		return false;
 	}
-	moves->data = malloc(sizeof(Move) * initial_capacity);
-	if (moves->data == NULL) {
-		free(moves);
-		return false;
-	}
-	moves->size = 0;
 	moves->_capacity = initial_capacity;
 	*list = moves;
 	return true;
@@ -28,7 +22,6 @@ bool move_list_create(MoveList **list) {
 
 void move_list_destroy(MoveList **list) {
 	if (list && *list != NULL) {
-		free((*list)->data);
 		free(*list);
 		*list = NULL;
 	}
@@ -44,21 +37,27 @@ size_t move_list_size(const MoveList *list) {
 	return list->size;
 }
 
-bool move_list_add(MoveList *list, Move move) {
+static bool _move_list_resize(MoveList **list) {
+	size_t new_cap = 2 * (*list)->_capacity;
+	MoveList *d = realloc(*list, sizeof(MoveList) + sizeof(Move) * new_cap);
+	if (d == NULL) {
+		return false;
+	}
+	(*list)->_capacity = new_cap;
+	*list = d;
+	return true;
+}
+
+bool move_list_add(MoveList **list, Move move) {
 	// this case should never happen under normal circumstances unless the
 	// user has modified the _capacity
-	assert(list->size <= list->_capacity);
+	assert((*list)->size <= (*list)->_capacity);
 
-	if (list->size == list->_capacity) {
-		list->_capacity *= 2;
-		Move *d = realloc(list->data, sizeof(Move) * list->_capacity);
-		if (d == NULL) {
-			return false;
-		}
-		list->data = d;
+	if ((*list)->size == (*list)->_capacity && !_move_list_resize(list)) {
+		return false;
 	}
-	list->data[list->size] = move;
-	list->size++;
+	(*list)->data[(*list)->size] = move;
+	(*list)->size++;
 	return true;
 }
 
@@ -83,7 +82,7 @@ Move move_list_get(const MoveList *list, size_t index) {
 void move_list_remove(MoveList *list, size_t index) {
 	assert(list != NULL);
 	assert(index < list->size);
-	for (unsigned int i = index; i < list->size; i++) {
+	for (size_t i = index; i < list->size; i++) {
 		list->data[i] = list->data[i + 1];
 	}
 	list->size--;
--- tests/move_test.c
+++ tests/move_test.c
@@ -40,44 +40,44 @@ Test(moves, create_moves_list_has_correct_capacity, .init = setup, .fini = teard
 
 Test(moves, add_move_adds_move_to_list, .init = setup, .fini = teardown) {
 	cr_assert(eq(uint, moves->size, 0));
-	bool added = move_list_add(moves, move_create(1, 1, 1, 1));
+	bool added = move_list_add(&moves, move_create(1, 1, 1, 1));
 	cr_assert_eq(added, true);
 	cr_assert(eq(uint, moves->size, 1));
 }
 
 Test(moves, get_moves_list_size_returns_correct_size, .init = setup, .fini = teardown) {
 	cr_assert(eq(uint, move_list_size(moves), 0));
-	move_list_add(moves, move_create(1, 1, 1, 1));
+	move_list_add(&moves, move_create(1, 1, 1, 1));
 	cr_assert(eq(uint, move_list_size(moves), 1));
 }
 
 Test(moves, clear_list_removes_all_moves, .init = setup, .fini = teardown) {
-	move_list_add(moves, move_create(1, 1, 1, 1));
-	move_list_add(moves, move_create(2, 2, 2, 2));
-	move_list_add(moves, move_create(3, 3, 3, 3));
+	move_list_add(&moves, move_create(1, 1, 1, 1));
+	move_list_add(&moves, move_create(2, 2, 2, 2));
+	move_list_add(&moves, move_create(3, 3, 3, 3));
 	move_list_clear(moves);
 	cr_assert(eq(uint, moves->size, 0));
 	cr_assert_not_null(moves);
 }
 
 Test(moves, is_move_in_list_returns_true_for_move_in_list, .init = setup, .fini = teardown) {
-	move_list_add(moves, move_create(1, 1, 1, 1));
-	move_list_add(moves, move_create(2, 2, 2, 2));
-	move_list_add(moves, move_create(3, 3, 3, 3));
+	move_list_add(&moves, move_create(1, 1, 1, 1));
+	move_list_add(&moves, move_create(2, 2, 2, 2));
+	move_list_add(&moves, move_create(3, 3, 3, 3));
 	bool is_present = move_list_contains(moves, move_create(2, 2, 2, 2));
 	cr_assert(is_present);
 }
 
 Test(moves, is_move_in_list_returns_false_for_move_not_in_list, .init = setup, .fini = teardown) {
-	move_list_add(moves, move_create(1, 1, 1, 1));
+	move_list_add(&moves, move_create(1, 1, 1, 1));
 	bool is_present = move_list_contains(moves, move_create(2, 2, 2, 2));
 	cr_assert_not(is_present);
 }
 
 Test(moves, get_move_at_index_returns_correct_move, .init = setup, .fini = teardown) {
-	move_list_add(moves, move_create(1, 1, 1, 1));
-	move_list_add(moves, move_create(2, 2, 2, 2));
-	move_list_add(moves, move_create(3, 3, 3, 3));
+	move_list_add(&moves, move_create(1, 1, 1, 1));
+	move_list_add(&moves, move_create(2, 2, 2, 2));
+	move_list_add(&moves, move_create(3, 3, 3, 3));
 	Move move = move_list_get(moves, 1);
 	cr_assert_eq(move.x_src, 2);
 	cr_assert_eq(move.y_src, 2);
@@ -86,9 +86,9 @@ Test(moves, get_move_at_index_returns_correct_move, .init = setup, .fini = teard
 }
 
 Test(moves, remove_move_at_index_removes_item_at_the_end, .init = setup, .fini = teardown) {
-	move_list_add(moves, move_create(1, 1, 1, 1));
-	move_list_add(moves, move_create(2, 2, 2, 2));
-	move_list_add(moves, move_create(3, 3, 3, 3));
+	move_list_add(&moves, move_create(1, 1, 1, 1));
+	move_list_add(&moves, move_create(2, 2, 2, 2));
+	move_list_add(&moves, move_create(3, 3, 3, 3));
 	move_list_remove(moves, 2);
 	cr_assert(eq(uint, move_list_size(moves), 2));
 	bool is_present = move_list_contains(moves, move_create(3, 3, 3, 3));
@@ -96,11 +96,39 @@ Test(moves, remove_move_at_index_removes_item_at_the_end, .init = setup, .fini =
 }
 
 Test(moves, remove_move_at_index_removes_item_at_the_start, .init = setup, .fini = teardown) {
-	move_list_add(moves, move_create(1, 1, 1, 1));
-	move_list_add(moves, move_create(2, 2, 2, 2));
-	move_list_add(moves, move_create(3, 3, 3, 3));
+	move_list_add(&moves, move_create(1, 1, 1, 1));
+	move_list_add(&moves, move_create(2, 2, 2, 2));
+	move_list_add(&moves, move_create(3, 3, 3, 3));
 	move_list_remove(moves, 0);
 	cr_assert(eq(uint, move_list_size(moves), 2));
 	bool is_present = move_list_contains(moves, move_create(1, 1, 1, 1));
 	cr_assert_eq(is_present, false);
+}
+
+Test(moves, move_list_resizes_correctly, .init = setup, .fini = teardown) {
+	move_list_add(&moves, move_create(1, 1, 1, 1));
+	move_list_add(&moves, move_create(2, 2, 2, 2));
+	move_list_add(&moves, move_create(3, 3, 3, 3));
+	move_list_add(&moves, move_create(4, 4, 4, 4));
+	move_list_add(&moves, move_create(5, 5, 5, 5));
+	move_list_add(&moves, move_create(6, 6, 6, 6));
+	move_list_add(&moves, move_create(7, 7, 7, 7));
+	move_list_add(&moves, move_create(8, 8, 8, 8));
+	move_list_add(&moves, move_create(9, 9, 9, 9));
+	move_list_add(&moves, move_create(10, 10, 10, 10));
+	move_list_add(&moves, move_create(11, 11, 11, 11));
+	move_list_add(&moves, move_create(12, 12, 12, 12));
+	move_list_add(&moves, move_create(13, 13, 13, 13));
+	move_list_add(&moves, move_create(14, 14, 14, 14));
+	move_list_add(&moves, move_create(15, 15, 15, 15));
+	move_list_add(&moves, move_create(16, 16, 16, 16));
+	move_list_add(&moves, move_create(17, 17, 17, 17));
+	cr_assert_eq(move_list_size(moves), 17);
+	for (int i = 0; i < 17; i++) {
+		Move m = move_list_get(moves, i);
+		cr_assert_eq(m.x_src, i + 1);
+		cr_assert_eq(m.y_src, i + 1);
+		cr_assert_eq(m.x_dest, i + 1);
+		cr_assert_eq(m.y_dest, i + 1);
+	}
 }

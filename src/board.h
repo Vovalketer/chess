82c7761587c44021dcf8224f0764cca1954fbc21
Vovalketer <kevinaa021@gmail.com>
2025-07-24 02:54:50 -0300

    feat(board): add history and some convenience functions

--- src/board.h
+++ src/board.h
@@ -1,37 +1,41 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#include "array_tpl.h"
 #include "types.h"
+ARRAY_DEFINE_TYPE(History, HistoryList, history)
 
 // TODO: add zobrist key
 // we must expose the board because the speed of the movegen depends on bit manipulation
 
 struct Board {
-	uint64_t pieces[2][6];	  // bitboards, Player and Piece used as index
-	uint64_t occupancies[2];  // white - black
-	uint16_t fullmove_counter;
-	uint8_t	 castling_rights;
-	int8_t	 ep_target;	 // ep key, will be set to -1 if no en passant target
-	Player	 side;
-	uint8_t	 halfmove_clock;
-	// 7 bytes padding
+	uint64_t	 pieces[2][6];	  // bitboards, Player and Piece used as index
+	uint64_t	 occupancies[2];  // white - black
+	uint16_t	 fullmove_counter;
+	uint8_t		 castling_rights;
+	uint8_t		 halfmove_clock;
+	Square		 ep_target;	 // will be set to -1 if no en passant target
+	Player		 side;
+	HistoryList *history;  // we could infer the ply from the size of the list
+
 	// uint64_t zobrist_key;
 };
 
 Board *board_create(void);
 void   board_destroy(Board **board);
 
-bool   board_set_piece(Board *board, Player player, PieceType piece, Square sqr);
-void   board_remove_piece(Board *board, Square sqr);
-Piece  board_get_piece(const Board *board, Square sqr);
-Piece  board_create_piece(Player player, PieceType piece);
-Player board_get_opponent(Player player);
-bool   board_has_enemy(const Board *board, Square sqr, Player player);
-bool   board_has_ally(const Board *board, Square sqr, Player player);
-bool   board_is_occupied(const Board *board, Square sqr);
-Player board_get_occupant(const Board *board, Square sqr);
-bool   board_has_castling_rights(const Board *board, CastlingRights cr);
-
-void board_print(const Board *board);
+void	  board_set_piece(Board *board, Player player, PieceType piece, Square sqr);
+void	  board_remove_piece(Board *board, Square sqr);
+void	  board_move_piece(Board *board, Square from, Square to, PieceType piece);
+PieceType board_get_piece_type(const Board *board, Square sqr);
+Piece	  board_create_piece(Player player, PieceType piece);
+Player	  board_get_opponent(Player player);
+bool	  board_has_enemy(const Board *board, Square sqr, Player player);
+bool	  board_has_ally(const Board *board, Square sqr, Player player);
+bool	  board_is_occupied(const Board *board, Square sqr);
+Player	  board_get_occupant(const Board *board, Square sqr);
+bool	  board_has_castling_rights(const Board *board, CastlingRights cr);
+void	  board_remove_castling_rights(Board *board, CastlingRights cr);
+void	  board_print(const Board *board);
 
 #endif	// BOARD_H

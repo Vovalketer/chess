a32dce0073a23fe2b5e95a0dcafcdc3de309bdf8
Vovalketer <kevinaa021@gmail.com>
2025-07-28 02:24:30 -0300

    refactor(types): change the piece definition

--- src/types.h
+++ src/types.h
@@ -56,20 +56,9 @@ typedef enum {
 } PieceType;
 
 // query results
-typedef enum {
-	PIECE_NONE,
-	W_PAWN,
-	W_ROOK,
-	W_KNIGHT,
-	W_BISHOP,
-	W_QUEEN,
-	W_KING,
-	B_PAWN,
-	B_ROOK,
-	B_KNIGHT,
-	B_BISHOP,
-	B_QUEEN,
-	B_KING,
+typedef struct {
+	PieceType type;
+	Player	  player;
 } Piece;
 
 typedef enum {

dba1a400f7672ab4d22f7d3be00478e9111be001
Vovalketer <kevinaa021@gmail.com>
2025-07-30 07:36:15 -0300

    tests(fen_test): migrate fen_test to unity

--- tests/fen_test.c
+++ tests/fen_test.c
@@ -1,231 +1,305 @@
-#include "fen.h"
+#include "../src/fen.h"
 
-#include "criterion/criterion.h"
-#include "criterion/new/assert.h"
-#include "game_state.h"
+#include <stdint.h>
+
+#include "../external/unity/unity.h"
+#include "../src/board.h"
+#include "../src/utils.h"
 #include "log.h"
 
-GameState* state = NULL;
-int prev_log_level;
+Board* board = NULL;
+int	   prev_log_level;
 
-void setup(void) {
+void setUp(void) {
 	prev_log_level = log_get_level();
 	log_set_level(LOG_NONE);
-	gstate_create_empty(&state);
+	board = board_create();
 }
 
-void teardown(void) {
+void tearDown(void) {
 	log_set_level(prev_log_level);
-	gstate_destroy(&state);
-	state = NULL;
+	board_destroy(&board);
 }
 
-TestSuite(fen, .init = setup, .fini = teardown);
-
-Test(fen, parse_empty_board) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/8 w - - 0 1", state);
-	cr_assert_eq(parsed, true);
+void test_parse_empty_board(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/8 w - - 0 1", board);
+	TEST_ASSERT_TRUE(parsed);
 
 	for (int i = 0; i < 8; i++) {
 		for (int j = 0; j < 8; j++) {
-			Position pos = (Position) {.x = i, .y = j};
-			Piece p = gstate_get_piece(state, pos);
-			cr_assert_eq(p.type, EMPTY);
-		}
-	}
-}
-
-Test(fen, parse_board_with_initial_setup) {
-	GameState* standard = NULL;
-	gstate_create(&standard);
-
-	bool parsed = fen_parse("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", state);
-	cr_assert_eq(parsed, true);
-
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++) {
-			Position pos = (Position) {.x = i, .y = j};
-			Piece fen_piece = gstate_get_piece(state, pos);
-			Piece standard_piece = gstate_get_piece(standard, pos);
-			cr_assert_eq(fen_piece.type, standard_piece.type);
-			cr_assert_eq(fen_piece.player, standard_piece.player);
+			Square sqr = utils_fr_to_square(i, j);
+			Piece  p   = board_get_piece(board, sqr);
+			TEST_ASSERT_EQUAL(p.type, EMPTY);
 		}
 	}
 
-	gstate_destroy(&standard);
+	TEST_ASSERT_EQUAL(board->side, PLAYER_W);
+	TEST_ASSERT_EQUAL(board->ep_target, SQ_NONE);
+	TEST_ASSERT_EQUAL(board->castling_rights, 0);
+	TEST_ASSERT_EQUAL(board->halfmove_clock, 0);
+	TEST_ASSERT_EQUAL(board->fullmove_counter, 1);
 }
 
-Test(fen, parse_board_with_castling_rights) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/8 w KQkq - 0 1", state);
-	cr_assert_eq(parsed, true);
-	cr_assert(gstate_has_castling_rights_kingside(state, WHITE_PLAYER));
-	cr_assert(gstate_has_castling_rights_queenside(state, WHITE_PLAYER));
-	cr_assert(gstate_has_castling_rights_kingside(state, BLACK_PLAYER));
-	cr_assert(gstate_has_castling_rights_queenside(state, BLACK_PLAYER));
+void test_parse_initial_setup(void) {
+	bool parsed = fen_parse("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", board);
+	TEST_ASSERT_TRUE(parsed);
+
+	Piece w_lrook = board_get_piece(board, SQ_A1);
+	TEST_ASSERT_EQUAL(w_lrook.type, ROOK);
+	TEST_ASSERT_EQUAL(w_lrook.player, PLAYER_W);
+
+	Piece w_lknight = board_get_piece(board, SQ_B1);
+	TEST_ASSERT_EQUAL(w_lknight.type, KNIGHT);
+	TEST_ASSERT_EQUAL(w_lknight.player, PLAYER_W);
+
+	Piece w_lbishop = board_get_piece(board, SQ_C1);
+	TEST_ASSERT_EQUAL(w_lbishop.type, BISHOP);
+	TEST_ASSERT_EQUAL(w_lbishop.player, PLAYER_W);
+
+	Piece w_queen = board_get_piece(board, SQ_D1);
+	TEST_ASSERT_EQUAL(w_queen.type, QUEEN);
+	TEST_ASSERT_EQUAL(w_queen.player, PLAYER_W);
+
+	Piece w_king = board_get_piece(board, SQ_E1);
+	TEST_ASSERT_EQUAL(w_king.type, KING);
+	TEST_ASSERT_EQUAL(w_king.player, PLAYER_W);
+
+	Piece w_rbishop = board_get_piece(board, SQ_F1);
+	TEST_ASSERT_EQUAL(w_rbishop.type, BISHOP);
+	TEST_ASSERT_EQUAL(w_rbishop.player, PLAYER_W);
+
+	Piece w_rknight = board_get_piece(board, SQ_G1);
+	TEST_ASSERT_EQUAL(w_rknight.type, KNIGHT);
+	TEST_ASSERT_EQUAL(w_rknight.player, PLAYER_W);
+
+	Piece w_rrook = board_get_piece(board, SQ_H1);
+	TEST_ASSERT_EQUAL(w_rrook.type, ROOK);
+	TEST_ASSERT_EQUAL(w_rrook.player, PLAYER_W);
+
+	for (int rank = 2; rank < 6; rank++) {
+		for (int file = 0; file < 8; file++) {
+			Square sqr	 = utils_fr_to_square(file, rank);
+			Piece  piece = board_get_piece(board, sqr);
+			TEST_ASSERT_EQUAL(piece.type, EMPTY);
+		}
+	}
+
+	Piece b_lrook = board_get_piece(board, SQ_A8);
+	TEST_ASSERT_EQUAL(b_lrook.type, ROOK);
+	TEST_ASSERT_EQUAL(b_lrook.player, PLAYER_B);
+
+	Piece b_lknight = board_get_piece(board, SQ_B8);
+	TEST_ASSERT_EQUAL(b_lknight.type, KNIGHT);
+	TEST_ASSERT_EQUAL(b_lknight.player, PLAYER_B);
+
+	Piece b_lbishop = board_get_piece(board, SQ_C8);
+	TEST_ASSERT_EQUAL(b_lbishop.type, BISHOP);
+	TEST_ASSERT_EQUAL(b_lbishop.player, PLAYER_B);
+
+	Piece b_queen = board_get_piece(board, SQ_D8);
+	TEST_ASSERT_EQUAL(b_queen.type, QUEEN);
+	TEST_ASSERT_EQUAL(b_queen.player, PLAYER_B);
+
+	Piece b_king = board_get_piece(board, SQ_E8);
+	TEST_ASSERT_EQUAL(b_king.type, KING);
+	TEST_ASSERT_EQUAL(b_king.player, PLAYER_B);
+
+	Piece b_rbishop = board_get_piece(board, SQ_F8);
+	TEST_ASSERT_EQUAL(b_rbishop.type, BISHOP);
+	TEST_ASSERT_EQUAL(b_rbishop.player, PLAYER_B);
+
+	Piece b_rknight = board_get_piece(board, SQ_G8);
+	TEST_ASSERT_EQUAL(b_rknight.type, KNIGHT);
+	TEST_ASSERT_EQUAL(b_rknight.player, PLAYER_B);
+
+	Piece b_rrook = board_get_piece(board, SQ_H8);
+	TEST_ASSERT_EQUAL(b_rrook.type, ROOK);
+	TEST_ASSERT_EQUAL(b_rrook.player, PLAYER_B);
 }
 
-Test(fen, parse_no_white_castling_rights) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w kq - 0 1", state);
-	cr_assert_eq(parsed, true);
-	cr_assert_not(gstate_has_castling_rights_kingside(state, WHITE_PLAYER));
-	cr_assert_not(gstate_has_castling_rights_queenside(state, WHITE_PLAYER));
-	cr_assert(gstate_has_castling_rights_kingside(state, BLACK_PLAYER));
-	cr_assert(gstate_has_castling_rights_queenside(state, BLACK_PLAYER));
+void test_parse_all_castling_rights(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/8 w KQkq - 0 1", board);
+	TEST_ASSERT_EQUAL(parsed, true);
+	TEST_ASSERT_EQUAL(board->castling_rights, CASTLING_ALL_RIGHTS);
 }
 
-Test(fen, parse_no_black_castling_rights) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/2R1K2R b KQ - 0 1", state);
-	cr_assert_eq(parsed, true);
-	cr_assert(gstate_has_castling_rights_kingside(state, WHITE_PLAYER));
-	cr_assert(gstate_has_castling_rights_queenside(state, WHITE_PLAYER));
-	cr_assert_not(gstate_has_castling_rights_kingside(state, BLACK_PLAYER));
-	cr_assert_not(gstate_has_castling_rights_queenside(state, BLACK_PLAYER));
+void test_parse_white_no_castling_rights(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w kq - 0 1", board);
+	TEST_ASSERT_EQUAL(parsed, true);
+	TEST_ASSERT_EQUAL(board->castling_rights, CASTLING_BLACK_ALL);
 }
 
-Test(fen, parse_no_castling_rights) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R b - - 0 1", state);
-	cr_assert_eq(parsed, true);
-	cr_assert_not(gstate_has_castling_rights_kingside(state, WHITE_PLAYER));
-	cr_assert_not(gstate_has_castling_rights_queenside(state, WHITE_PLAYER));
-	cr_assert_not(gstate_has_castling_rights_kingside(state, BLACK_PLAYER));
-	cr_assert_not(gstate_has_castling_rights_queenside(state, BLACK_PLAYER));
+void test_parse_no_black_castling_rights(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/2R1K2R b KQ - 0 1", board);
+	TEST_ASSERT_EQUAL(parsed, true);
+	TEST_ASSERT_EQUAL(board->castling_rights, CASTLING_WHITE_ALL);
 }
 
-Test(fen, parse_kingside_castling_rights) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Kk - 0 1", state);
-	cr_assert_eq(parsed, true);
-	cr_assert(gstate_has_castling_rights_kingside(state, WHITE_PLAYER));
-	cr_assert_not(gstate_has_castling_rights_queenside(state, WHITE_PLAYER));
-	cr_assert(gstate_has_castling_rights_kingside(state, BLACK_PLAYER));
-	cr_assert_not(gstate_has_castling_rights_queenside(state, BLACK_PLAYER));
+void test_parse_no_castling_rights(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R b - - 0 1", board);
+	TEST_ASSERT_EQUAL(parsed, true);
+	TEST_ASSERT_EQUAL(board->castling_rights, CASTLING_NO_RIGHTS);
 }
 
-Test(fen, parse_queenside_castling_rights) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Qq - 0 1", state);
-	cr_assert_eq(parsed, true);
-	cr_assert_not(gstate_has_castling_rights_kingside(state, WHITE_PLAYER));
-	cr_assert(gstate_has_castling_rights_queenside(state, WHITE_PLAYER));
-	cr_assert_not(gstate_has_castling_rights_kingside(state, BLACK_PLAYER));
-	cr_assert(gstate_has_castling_rights_queenside(state, BLACK_PLAYER));
+void test_parse_kingside_castling_rights(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Kk - 0 1", board);
+	TEST_ASSERT_EQUAL(parsed, true);
+	TEST_ASSERT_EQUAL(board->castling_rights, CASTLING_WHITE_KS | CASTLING_BLACK_KS);
 }
 
-Test(fen, parse_invalid_castling_rights) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w tq - 0 1", state);
-	cr_assert_eq(parsed, false);
+void test_parse_queenside_castling_rights(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Qq - 0 1", board);
+	TEST_ASSERT_EQUAL(parsed, true);
+	TEST_ASSERT_EQUAL(board->castling_rights, CASTLING_WHITE_QS | CASTLING_BLACK_QS);
 }
 
-Test(fen, parse_en_passant) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Qq e6 0 1", state);
-	cr_assert_eq(parsed, true);
-	Position expected = (Position) {4, 2};
-	Position actual = gstate_get_en_passant_target(state);
-	cr_assert(eq(int, expected.x, actual.x));
-	cr_assert(eq(int, expected.y, actual.y));
+void test_parse_invalid_castling_rights(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w tq - 0 1", board);
+	TEST_ASSERT_EQUAL(parsed, false);
 }
 
-Test(fen, parse_en_passant_top_right_corner) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Qq h8 0 1", state);
-	cr_assert_eq(parsed, true);
-	Position expected = (Position) {7, 0};
-	Position actual = gstate_get_en_passant_target(state);
-	cr_assert(eq(int, expected.x, actual.x));
-	cr_assert(eq(int, expected.y, actual.y));
+void test_parse_en_passant(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Qq e6 0 1", board);
+	TEST_ASSERT_EQUAL(parsed, true);
+	Square expected = SQ_E6;
+	Square actual	= board->ep_target;
+	TEST_ASSERT_EQUAL(expected, actual);
 }
 
-Test(fen, parse_en_passant_bottom_left_corner) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/2R1K2R b Qq a1 0 1", state);
-	cr_assert_eq(parsed, true);
-	Position expected = (Position) {0, 7};
-	Position actual = gstate_get_en_passant_target(state);
-	cr_assert(eq(int, expected.x, actual.x));
-	cr_assert(eq(int, expected.y, actual.y));
+void test_parse_en_passant_top_right_corner(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Qq h8 0 1", board);
+	TEST_ASSERT_EQUAL(parsed, true);
+	Square expected = SQ_H8;
+	Square actual	= board->ep_target;
+	TEST_ASSERT_EQUAL(expected, actual);
 }
 
-Test(fen, parse_en_passant_invalid_number) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Qq e0 0 1", state);
-	cr_assert_eq(parsed, false);
+void test_parse_en_passant_bottom_left_corner(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/2R1K2R b Qq a1 0 1", board);
+	TEST_ASSERT_EQUAL(parsed, true);
+	Square expected = SQ_A1;
+	Square actual	= board->ep_target;
+	TEST_ASSERT_EQUAL(expected, actual);
 }
 
-Test(fen, parse_en_passant_out_of_bounds_number) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Qq e9 0 1", state);
-	cr_assert_eq(parsed, false);
+void test_parse_en_passant_invalid_number(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Qq e0 0 1", board);
+	TEST_ASSERT_EQUAL(parsed, false);
 }
 
-Test(fen, parse_en_passant_invalid_letter) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Qq i2 0 1", state);
-	cr_assert_eq(parsed, false);
+void test_parse_en_passant_out_of_bounds_number(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Qq e9 0 1", board);
+	TEST_ASSERT_EQUAL(parsed, false);
 }
 
-Test(fen, parse_halfmove_clock_zero) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Qq e6 0 1", state);
-	cr_assert_eq(parsed, true);
-	cr_assert_eq(gstate_get_halfmove_clock(state), 0);
+void test_parse_en_passant_invalid_letter(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Qq i2 0 1", board);
+	TEST_ASSERT_EQUAL(parsed, false);
 }
 
-Test(fen, parse_halfmove_clock_one) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Qq e6 1 1", state);
-	cr_assert_eq(parsed, true);
-	cr_assert_eq(gstate_get_halfmove_clock(state), 1);
+void test_parse_halfmove_clock_zero(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Qq e6 0 1", board);
+	TEST_ASSERT_EQUAL(parsed, true);
+	TEST_ASSERT_EQUAL(board->halfmove_clock, 0);
 }
 
-Test(fen, parse_halfmove_clock_hundred) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Qq e6 100 1", state);
-	cr_assert_eq(parsed, true);
-	cr_assert_eq(gstate_get_halfmove_clock(state), 100);
+void test_parse_halfmove_clock_one(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Qq e6 1 1", board);
+	TEST_ASSERT_EQUAL(parsed, true);
+	TEST_ASSERT_EQUAL(board->halfmove_clock, 1);
 }
 
-Test(fen, parse_halfmove_clock_invalid) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Qq e6 1a0 1", state);
-	cr_assert_eq(parsed, false);
+void test_parse_halfmove_clock_hundred(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Qq e6 100 1", board);
+	TEST_ASSERT_EQUAL(parsed, true);
+	TEST_ASSERT_EQUAL(board->halfmove_clock, 100);
 }
 
-Test(fen, parse_active_player_white) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Kk e6 0 1", state);
-	cr_assert_eq(parsed, true);
-	cr_assert_eq(gstate_get_player_turn(state), WHITE_PLAYER);
+void test_parse_halfmove_clock_invalid(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Qq e6 1a0 1", board);
+	TEST_ASSERT_EQUAL(parsed, false);
 }
 
-Test(fen, parse_active_player_black) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R b Kk e6 1 1", state);
-	cr_assert_eq(parsed, true);
-	cr_assert_eq(gstate_get_player_turn(state), BLACK_PLAYER);
+void test_parse_active_player_white(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Kk e6 0 1", board);
+	TEST_ASSERT_EQUAL(parsed, true);
+	TEST_ASSERT_EQUAL(board->side, PLAYER_W);
 }
 
-Test(fen, parse_active_player_invalid_char) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R n Kk e6 1 1", state);
-	cr_assert_eq(parsed, false);
+void test_parse_active_player_black(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R b Kk e6 1 1", board);
+	TEST_ASSERT_EQUAL(parsed, true);
+	TEST_ASSERT_EQUAL(board->side, PLAYER_B);
 }
 
-Test(fen, parse_active_player_invalid_int) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R 2 Kk e6 1 1", state);
-	cr_assert_eq(parsed, false);
+void test_parse_active_player_invalid_char(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R n Kk e6 1 1", board);
+	TEST_ASSERT_EQUAL(parsed, false);
 }
 
-Test(fen, parse_fullmove_counter_zero) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Kk e6 1 0", state);
-	cr_assert_eq(parsed, true);
-	cr_assert_eq(gstate_get_fullmove_counter(state), 0);
+void test_parse_active_player_invalid_int(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R 2 Kk e6 1 1", board);
+	TEST_ASSERT_EQUAL(parsed, false);
 }
 
-Test(fen, parse_fullmove_counter_one) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Kk e6 1 1", state);
-	cr_assert_eq(parsed, true);
-	cr_assert_eq(gstate_get_fullmove_counter(state), 1);
+void test_parse_fullmove_counter_zero(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Kk e6 1 0", board);
+	TEST_ASSERT_EQUAL(parsed, true);
+	TEST_ASSERT_EQUAL(board->fullmove_counter, 0);
 }
 
-Test(fen, parse_fullmove_counter_prime) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Kk e6 0 13", state);
-	cr_assert_eq(parsed, true);
-	cr_assert_eq(gstate_get_fullmove_counter(state), 13);
+void test_parse_fullmove_counter_one(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Kk e6 1 1", board);
+	TEST_ASSERT_EQUAL(parsed, true);
+	TEST_ASSERT_EQUAL(board->fullmove_counter, 1);
 }
 
-Test(fen, parse_fullmove_counter_hundred) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Kk e6 10 100", state);
-	cr_assert_eq(parsed, true);
-	cr_assert_eq(gstate_get_fullmove_counter(state), 100);
+void test_parse_fullmove_counter_prime(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Kk e6 0 13", board);
+	TEST_ASSERT_EQUAL(parsed, true);
+	TEST_ASSERT_EQUAL(board->fullmove_counter, 13);
 }
 
-Test(fen, parse_fullmove_counter_invalid) {
-	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Kk e6 10 1a1", state);
-	cr_assert_eq(parsed, false);
+void test_parse_fullmove_counter_hundred(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Kk e6 10 100", board);
+	TEST_ASSERT_EQUAL(parsed, true);
+	TEST_ASSERT_EQUAL(board->fullmove_counter, 100);
+}
+
+void test_parse_fullmove_counter_invalid(void) {
+	bool parsed = fen_parse("8/8/8/8/8/8/8/R3K2R w Kk e6 10 1a1", board);
+	TEST_ASSERT_EQUAL(parsed, false);
+}
+
+int main(void) {
+	UNITY_BEGIN();
+	RUN_TEST(test_parse_empty_board);
+	RUN_TEST(test_parse_initial_setup);
+	RUN_TEST(test_parse_all_castling_rights);
+	RUN_TEST(test_parse_white_no_castling_rights);
+	RUN_TEST(test_parse_no_black_castling_rights);
+	RUN_TEST(test_parse_no_castling_rights);
+	RUN_TEST(test_parse_kingside_castling_rights);
+	RUN_TEST(test_parse_queenside_castling_rights);
+	RUN_TEST(test_parse_invalid_castling_rights);
+	RUN_TEST(test_parse_en_passant);
+	RUN_TEST(test_parse_en_passant_top_right_corner);
+	RUN_TEST(test_parse_en_passant_bottom_left_corner);
+	RUN_TEST(test_parse_en_passant_invalid_number);
+	RUN_TEST(test_parse_en_passant_out_of_bounds_number);
+	RUN_TEST(test_parse_en_passant_invalid_letter);
+	RUN_TEST(test_parse_halfmove_clock_zero);
+	RUN_TEST(test_parse_halfmove_clock_one);
+	RUN_TEST(test_parse_halfmove_clock_hundred);
+	RUN_TEST(test_parse_halfmove_clock_invalid);
+	RUN_TEST(test_parse_active_player_white);
+	RUN_TEST(test_parse_active_player_black);
+	RUN_TEST(test_parse_active_player_invalid_char);
+	RUN_TEST(test_parse_active_player_invalid_int);
+	RUN_TEST(test_parse_fullmove_counter_zero);
+	RUN_TEST(test_parse_fullmove_counter_one);
+	RUN_TEST(test_parse_fullmove_counter_prime);
+	RUN_TEST(test_parse_fullmove_counter_hundred);
+	RUN_TEST(test_parse_fullmove_counter_invalid);
+	return UNITY_END();
 }

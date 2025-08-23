8934c5dcc4d9a607c23f244fbd0bb2b3935e3592
Vovalketer <kevinaa021@gmail.com>
2025-08-23 01:08:05 -0300

    fix(movelist): contains logic

--- src/engine/movelist.c
+++ src/engine/movelist.c
@@ -3,12 +3,13 @@
 #include <assert.h>
 #include <stddef.h>
 
+#include "types.h"
+
 bool move_list_contains(MoveList *list, Move move) {
 	assert(list != NULL);
 	for (size_t i = 0; i < move_list_size(list); i++) {
 		Move *m = move_list_get(list, i);
-		if (m->from == move.from && m->to == move.to && m->mv_type == move.mv_type &&
-			m->piece == move.piece) {
+		if (move_equals(move, *m)) {
 			return true;
 		}
 	}

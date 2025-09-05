3e2d2ee266bd3650b6791272d05bfd953ccb6d10
Vovalketer <kevinaa021@gmail.com>
2025-09-05 01:10:52 -0300

    refactor(search): simplify the logic

---
+++ src/engine/engine.h
@@ -0,0 +1,8 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+typedef struct {
+	int threads;
+} EngineConfig;
+
+#endif	// ENGINE_H
--- src/engine/search.c
+++ src/engine/search.c
@@ -3,6 +3,7 @@
 #include <limits.h>
 
 #include "board.h"
+#include "engine.h"
 #include "eval.h"
 #include "log.h"
 #include "makemove.h"
@@ -42,34 +43,24 @@ typedef struct {
 VECTOR_DEFINE_TYPE(ScoredMove, ScoredMoveList, sm)
 
 typedef struct {
-	Move		   pv_table[MAX_DEPTH][MAX_DEPTH];	// stack allocated matrix for fast access
-	uint8_t		   pv_length[MAX_DEPTH];
-	Move		   killer_moves[MAX_DEPTH][2];
-	int			   history_heuristic[2][SQ_CNT][SQ_CNT];  // player, from, to
-	ScoredMoveList scored_moves;						  // meant to be used as cache for sorting
-	int			   ply;
-	Player		   side;
-	uint64_t	   nodes;
+	int		 ply;
+	Player	 side;
+	uint64_t nodes;
 } SearchContext;
 
-static void search_ctx_init(SearchContext* ctx) {
-	memset(ctx, 0, sizeof(*ctx));
-	sm_init_reserve(&ctx->scored_moves, 128);
-}
-
-static void search_ctx_free(SearchContext* ctx) {
-	sm_free(&ctx->scored_moves);
-}
-
-static int mvv_lva_get(PieceType victim, PieceType attacker) {
-	return mvv_lva[attacker][victim];
-}
+volatile bool  stop = false;
+Move		   pv_table[MAX_DEPTH][MAX_DEPTH];
+uint8_t		   pv_length[MAX_DEPTH];
+Move		   killer_moves[MAX_DEPTH][2];
+int			   history_heuristic[PLAYER_CNT][SQ_CNT][SQ_CNT];  // player, from, to
+MoveList	   root_pv;
+ScoredMoveList scored_moves;
 
 static int mvv_lva_compare(const void* x, const void* y) {
 	Move* xt	= (Move*) x;
 	Move* yt	= (Move*) y;
-	int	  x_val = mvv_lva_get(xt->captured_type, xt->piece.type);
-	int	  y_val = mvv_lva_get(yt->captured_type, yt->piece.type);
+	int	  x_val = mvv_lva[xt->piece.type][xt->captured_type];
+	int	  y_val = mvv_lva[yt->piece.type][yt->captured_type];
 	if (x_val < y_val)
 		return 1;
 	else if (x_val > y_val)
@@ -101,19 +92,18 @@ static bool is_repetition(Board* board) {
 
 static int score_move(Move move, SearchContext* ctx) {
 	if (move.captured_type != EMPTY)
-		return mvv_lva_get(move.captured_type, move.piece.type) + SCORE_CAPTURE;
+		return mvv_lva[move.piece.type][move.captured_type] + SCORE_CAPTURE;
 
-	if (move_equals(move, ctx->killer_moves[ctx->ply][0]))
+	if (move_equals(move, killer_moves[ctx->ply][0]))
 		return SCORE_KILLER_FIRST;
-	else if (move_equals(move, ctx->killer_moves[ctx->ply][1]))
+	else if (move_equals(move, killer_moves[ctx->ply][1]))
 		return SCORE_KILLER_SECOND;
 	else
-		return ctx->history_heuristic[ctx->side][move.from][move.to] + SCORE_HISTORY;
+		return history_heuristic[ctx->side][move.from][move.to] + SCORE_HISTORY;
 }
 
 static ScoredMoveList get_scored_moves(MoveList* moves, SearchContext* ctx) {
-	ScoredMoveList scored_moves;
-	sm_init(&scored_moves);
+	sm_clear(&scored_moves);
 	for (size_t i = 0; i < move_list_size(moves); i++) {
 		Move move = *move_list_get(moves, i);
 		assert(move.from != move.to);
@@ -122,17 +112,6 @@ static ScoredMoveList get_scored_moves(MoveList* moves, SearchContext* ctx) {
 	}
 	sm_sort(&scored_moves, scored_move_compare);
 	return scored_moves;
-}
-
-void copy_pv(SearchContext* ctx, Move best_move, int ply) {
-	ctx->pv_table[ply][0] = best_move;
-
-	if (ctx->pv_length[ply + 1] > 0) {
-		memcpy(&ctx->pv_table[ply][1],
-			   &ctx->pv_table[ply + 1][0],
-			   sizeof(Move) * (ctx->pv_length[ply + 1]));
-	}
-	ctx->pv_length[ply] = 1 + ctx->pv_length[ply + 1];
 }
 
 int quiescence(SearchContext* ctx, Board* board, int alpha, int beta, int ply) {
@@ -161,25 +140,26 @@ int quiescence(SearchContext* ctx, Board* board, int alpha, int beta, int ply) {
 			return score;
 		if (score > val)
 			val = score;
-		if (score > alpha)
+		if (score > alpha) {
 			alpha = score;
+		}
 	}
 	move_list_destroy(&moves);
 	return val;
 }
 
 void copy_tt_entry(SearchContext* ctx, TEntry* entry) {
-	ctx->pv_table[ctx->ply][0] = entry->best_move;
-	ctx->pv_length[0]		   = 1;
+	pv_table[ctx->ply][0] = entry->best_move;
+	pv_length[0]		  = 1;
 }
 
 int alpha_beta(SearchContext* ctx, Board* board, int depth, int alpha, int beta, int ply) {
-	ctx->pv_length[ply]		  = 0;
-	ctx->side				  = board->side;
-	ctx->ply				  = ply;
-	ctx->killer_moves[ply][0] = NO_MOVE;
-	ctx->killer_moves[ply][1] = NO_MOVE;
-	sm_clear(&ctx->scored_moves);
+	pv_length[ply]		 = 0;
+	ctx->side			 = board->side;
+	ctx->ply			 = ply;
+	killer_moves[ply][0] = NO_MOVE;
+	killer_moves[ply][1] = NO_MOVE;
+	sm_clear(&scored_moves);
 
 	TEntry entry;
 	if (ttable_probe(board->hash, &entry)) {
@@ -216,12 +196,11 @@ int alpha_beta(SearchContext* ctx, Board* board, int depth, int alpha, int beta,
 
 	MoveList* moves = movegen_generate(board, board->side);
 
-	int	 original_alpha			= alpha;
-	int	 best_score				= -INF;
-	Move best_move				= NO_MOVE;
-	int	 legal_moves			= 0;
-	ctx->scored_moves			= get_scored_moves(moves, ctx);
-	ScoredMoveList scored_moves = ctx->scored_moves;
+	int	 original_alpha = alpha;
+	int	 best_score		= -INF;
+	Move best_move		= NO_MOVE;
+	int	 legal_moves	= 0;
+	scored_moves		= get_scored_moves(moves, ctx);
 
 	for (size_t i = 0; i < sm_size(&scored_moves); i++) {
 		ScoredMove sm = *sm_get(&scored_moves, i);
@@ -238,14 +217,14 @@ int alpha_beta(SearchContext* ctx, Board* board, int depth, int alpha, int beta,
 			best_score = score;
 			best_move  = sm.move;
 
-			ctx->pv_table[ply][0] = sm.move;
-			if (ctx->pv_length[ply + 1] > 0) {
+			pv_table[ply][0] = sm.move;
+			if (pv_length[ply + 1] > 0) {
 				// copy the child's PV
-				memcpy(&ctx->pv_table[ply][1],
-					   &ctx->pv_table[ply + 1][0],
-					   sizeof(ctx->pv_table[ply][0]) * ctx->pv_length[ply + 1]);
+				memcpy(&pv_table[ply][1],
+					   &pv_table[ply + 1][0],
+					   sizeof(pv_table[ply][0]) * pv_length[ply + 1]);
 			}
-			ctx->pv_length[ply] = 1 + ctx->pv_length[ply + 1];
+			pv_length[ply] = 1 + pv_length[ply + 1];
 		}
 		if (score > alpha) {
 			alpha = score;
@@ -253,11 +232,11 @@ int alpha_beta(SearchContext* ctx, Board* board, int depth, int alpha, int beta,
 		if (score >= beta) {
 			// killer heuristic
 			if (sm.move.captured_type == EMPTY) {
-				ctx->killer_moves[ply][1] = ctx->killer_moves[ply][0];
-				ctx->killer_moves[ply][0] = sm.move;
+				killer_moves[ply][1] = killer_moves[ply][0];
+				killer_moves[ply][0] = sm.move;
 			}
 			// history heuristic
-			ctx->history_heuristic[board->side][sm.move.from][sm.move.to] += depth * depth;
+			history_heuristic[board->side][sm.move.from][sm.move.to] += depth * depth;
 			break;
 		}
 	}
@@ -293,32 +272,53 @@ int alpha_beta(SearchContext* ctx, Board* board, int depth, int alpha, int beta,
 	return best_score;
 }
 
-Move search_best_move(Board* board, int max_depth) {
-	Move	 best_move;
-	MoveList root_pv;
-	move_list_init(&root_pv);
-	SearchContext ctx;
-	search_ctx_init(&ctx);
+void search_init(void) {
+	memset(pv_table, 0, sizeof(pv_table));
+	memset(pv_length, 0, sizeof(pv_length));
+	memset(killer_moves, 0, sizeof(killer_moves));
+	memset(history_heuristic, 0, sizeof(history_heuristic));
+	move_list_init_reserve(&root_pv, 32);
+	sm_init_reserve(&scored_moves, 32);
+}
+
+void search_reset(void) {
+	memset(pv_table, 0, sizeof(pv_table));
+	memset(pv_length, 0, sizeof(pv_length));
+	memset(killer_moves, 0, sizeof(killer_moves));
+	memset(history_heuristic, 0, sizeof(history_heuristic));
+	move_list_clear(&root_pv);
+	sm_clear(&scored_moves);
+}
+
+Move search_best_move(Board* board, SearchOptions* options, EngineConfig* cfg) {
+	SearchContext ctx = {0};
+	stop			  = false;
 
 	// iterative deepening
-	for (int depth = 1; depth <= max_depth; depth++) {
+	for (size_t depth = 1; depth <= options->depth; depth++) {
+		if (stop) {
+			break;
+		}
 		// reset pvs for this search
-		memset(&ctx.pv_length, 0, sizeof(ctx.pv_length));
+		memset(&pv_length, 0, sizeof(pv_length));
 
 		int score = alpha_beta(&ctx, board, depth, -INF, INF, 0);
 		log_info("info depth %d score %d nodes %llu", depth, score, ctx.nodes);
 		move_list_clear(&root_pv);
-		for (int i = 0; i < ctx.pv_length[0]; i++) {
-			move_list_push_back(&root_pv, ctx.pv_table[0][i]);
+		for (size_t i = 0; i < pv_length[0]; ++i) {
+			move_list_push_back(&root_pv, pv_table[0][i]);
 		}
-		if (move_list_size(&root_pv) > 0) {
-			best_move = *move_list_get(&root_pv, 0);
-		}
+
 		log_info("PV:");
 		for (size_t i = 0; i < move_list_size(&root_pv); i++) {
 			log_info("%s", utils_move_description(*move_list_get(&root_pv, i)).str);
 		}
 	}
-	search_ctx_free(&ctx);
-	return best_move;
+	assert(move_list_size(&root_pv) > 0);
+	return *move_list_get(&root_pv, 0);
 }
+
+void search_stop(void) {
+	stop = true;
+}
+

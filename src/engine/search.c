d8788877bd10274d30e16007a4eaa34d5ba9ad7e
Vovalketer <kevinaa021@gmail.com>
2025-09-10 03:29:10 -0300

    feat(search): time control and PVS impl

--- src/engine/search.c
+++ src/engine/search.c
@@ -13,7 +13,8 @@
 #include "transposition.h"
 #include "types.h"
 #include "utils.h"
-#include "vector.h"
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 #define INF		  (INT_MAX - 100000)
 #define CHECKMATE (INF - 1000000)
@@ -30,17 +31,13 @@ static const int mvv_lva[PIECE_TYPE_CNT][PIECE_TYPE_CNT] = {
 };
 
 typedef enum {
-	SCORE_TT			= 20000,
-	SCORE_CAPTURE		= 10000,
-	SCORE_KILLER_FIRST	= 9000,
-	SCORE_KILLER_SECOND = 8000,
+	SCORE_NONE			= 0,
 	SCORE_HISTORY		= 5000,
+	SCORE_KILLER_SECOND = 8000,
+	SCORE_KILLER_FIRST	= 9000,
+	SCORE_CAPTURE		= 10000,
+	SCORE_TT			= 20000,
 } ScoreType;
-
-typedef struct {
-	Move move;
-	int	 score;
-} ScoredMove;
 
 typedef struct {
 	int		 ply;
@@ -51,10 +48,10 @@ typedef struct {
 } SearchContext;
 
 uint32_t time_now(void);
-bool	 search_should_end(SearchOptions* options,
-						   uint32_t		  nodes,
-						   uint32_t		  time_start,
-						   uint32_t		  time_limit);
+void	 gstop_cond_eval(SearchOptions* options,
+						 uint32_t		nodes,
+						 uint32_t		time_start,
+						 uint32_t		time_limit);
 
 Move		  pv_table[MAX_DEPTH][MAX_DEPTH];
 uint8_t		  pv_length[MAX_DEPTH];
@@ -100,7 +97,7 @@ static void score_move(Move* move, int ply, Player side, TEntry* tte) {
 	else if (history_heuristic[side][move->from][move->to] != 0)
 		move->score = history_heuristic[side][move->from][move->to] + SCORE_HISTORY;
 	else
-		move->score = 0;
+		move->score = SCORE_NONE;
 }
 
 static int compare_move(const void* m1, const void* m2) {
@@ -142,10 +139,12 @@ int quiescence(SearchContext* ctx, Board* board, int alpha, int beta, int ply) {
 		int score = -quiescence(ctx, board, -beta, -alpha, ply + 1);
 		unmake_move(board);
 
-		if (score >= beta)
-			break;
-		if (score > best_score)
-			best_score = score;
+		best_score = MAX(best_score, score);
+
+		if (score >= beta) {
+			move_list_destroy(&moves);
+			return score;
+		}
 		if (score > alpha)
 			alpha = score;
 	}
@@ -153,53 +152,60 @@ int quiescence(SearchContext* ctx, Board* board, int alpha, int beta, int ply) {
 	return best_score;
 }
 
-int alpha_beta(SearchContext* ctx,
-			   SearchOptions* opts,
-			   Board*		  board,
-			   int			  depth,
-			   int			  alpha,
-			   int			  beta,
-			   int			  ply) {
-	pv_length[ply]		 = 0;
-	ctx->side			 = board->side;
-	ctx->ply			 = ply;
-	killer_moves[ply][0] = NO_MOVE;
-	killer_moves[ply][1] = NO_MOVE;
+// PVS
+int search(int			  depth,
+		   int			  alpha,
+		   int			  beta,
+		   int			  ply,
+		   Board*		  board,
+		   SearchOptions* opts,
+		   SearchContext* ctx,
+		   bool			  is_pv) {
+	pv_length[ply] = 0;
+	ctx->side	   = board->side;
+	ctx->ply	   = ply;
 
 	if (depth == 0) {
 		return quiescence(ctx, board, alpha, beta, ply);
 	}
 
 	ctx->nodes++;
-
-	TEntry entry;
-	if (ttable_probe(board->hash, &entry) && entry.depth >= depth) {
-		if (entry.bound == BOUND_EXACT || (entry.bound == BOUND_LOWER && entry.score >= beta) ||
-			(entry.bound == BOUND_UPPER && entry.score <= alpha)) {
-			// TT hit, dont need to analyze any further
-			pv_table[ctx->ply][0] = entry.best_move;
-			pv_length[0]		  = 1;
-
-			return entry.score;
-		}
-	}
-
-	if (search_should_end(opts, ctx->nodes, ctx->time_start, ctx->time_limit))
-		return 0;
+	gstop_cond_eval(opts, ctx->nodes, ctx->time_start, ctx->time_limit);
 
 	if (board->halfmove_clock > 99 || is_repetition(board))
 		return 0;
+	if (stop)
+		return 0;
+
+	TEntry entry = {0};
+	if (ttable_probe(board->hash, &entry) && entry.depth >= depth) {
+		if (!is_pv) {
+			if (entry.bound == BOUND_EXACT) {
+				return entry.score;
+			} else if (entry.bound == BOUND_LOWER && entry.score >= beta) {
+				return beta;
+			} else if (entry.bound == BOUND_UPPER && entry.score <= alpha) {
+				return alpha;
+			}
+		} else {
+			if (entry.bound == BOUND_EXACT) {
+				pv_table[ctx->ply][0] = entry.best_move;
+				pv_length[ctx->ply]	  = 1;
+				return entry.score;
+			}
+		}
+	}
 
 	MoveList* moves = movegen_generate(board, board->side);
 
-	int	 original_alpha = alpha;
-	int	 best_score		= -INF;
-	Move best_move		= NO_MOVE;
-	int	 legal_moves	= 0;
+	Move	  best_move	  = NO_MOVE;
+	int		  best_score  = -INF;
+	BoundType tt_bound	  = BOUND_UPPER;  // default to score<=alpha
+	int		  legal_moves = 0;
 	sort_moves(moves, ctx, &entry);
 
 	for (size_t i = 0; i < move_list_size(moves); i++) {
-		if (search_should_end(opts, ctx->nodes, ctx->time_start, ctx->time_limit)) {
+		if (stop) {
 			move_list_destroy(&moves);
 			return 0;
 		}
@@ -207,27 +213,24 @@ int alpha_beta(SearchContext* ctx,
 		if (!make_move(board, mv)) {
 			continue;
 		}
-		int score = -alpha_beta(ctx, opts, board, depth - 1, -beta, -alpha, ply + 1);
 		legal_moves++;
-		unmake_move(board);
 
-		if (score > best_score) {
-			best_score = score;
-			best_move  = mv;
-
-			pv_table[ply][0] = mv;
-			pv_length[ply]	 = 1;
-			if (pv_length[ply + 1] > 0 && (ply + 1) < MAX_DEPTH) {
-				// copy the child's PV
-				memcpy(&pv_table[ply][1],
-					   &pv_table[ply + 1][0],
-					   sizeof(pv_table[ply][0]) * pv_length[ply + 1]);
-				pv_length[ply] += pv_length[ply + 1];
+		int score;
+		if (i == 0) {
+			// full width search on the first move
+			score = -search(depth - 1, -beta, -alpha, ply + 1, board, opts, ctx, is_pv);
+		} else {
+			// reduced width search
+			score = -search(depth - 1, -alpha - 1, -alpha, ply + 1, board, opts, ctx, false);
+			if (score > alpha && score < beta) {
+				// full width research
+				score = -search(depth - 1, -beta, -alpha, ply + 1, board, opts, ctx, is_pv);
 			}
 		}
-		if (score > alpha) {
-			alpha = score;
-		}
+
+		unmake_move(board);
+		best_score = MAX(best_score, score);
+
 		if (score >= beta) {
 			// killer heuristic
 			if (mv.captured_type == EMPTY) {
@@ -236,15 +239,36 @@ int alpha_beta(SearchContext* ctx,
 			}
 			// history heuristic
 			history_heuristic[board->side][mv.from][mv.to] += depth * depth;
+
+			// alpha	 = beta;
+			tt_bound = BOUND_LOWER;
 			break;
+		}
+
+		if (score > alpha) {
+			alpha	  = score;
+			best_move = mv;
+			tt_bound  = BOUND_EXACT;
+
+			pv_table[ply][0] = mv;
+			pv_length[ply]	 = 1;
+			if ((ply + 1) < MAX_DEPTH && pv_length[ply + 1] > 0) {
+				// copy the child's PV
+				memcpy(&pv_table[ply][1],
+					   &pv_table[ply + 1][0],
+					   sizeof(pv_table[ply][0]) * pv_length[ply + 1]);
+				pv_length[ply] += pv_length[ply + 1];
+			}
 		}
 	}
 	move_list_destroy(&moves);
 
+	int tt_score = best_score;
 	if (legal_moves == 0) {
 		if (board_is_check(board, board->side)) {
 			// shorter mate preferred
 			best_score = -CHECKMATE + ply;
+			tt_score   = -CHECKMATE;  // store normalized score
 		} else {
 			// stalemate
 			best_score = 0;
@@ -252,22 +276,10 @@ int alpha_beta(SearchContext* ctx,
 	}
 
 	if (!move_equals(best_move, NO_MOVE)) {
-		BoundType bound;
-		if (best_score <= original_alpha) {
-			bound = BOUND_UPPER;
-		} else if (best_score >= beta) {
-			bound = BOUND_LOWER;
-		} else {
-			bound = BOUND_EXACT;
-		}
-		// normalize checkmate score by removing the bias for shorter mates
-		int tt_score = best_score;
-		if (tt_score > CHECKMATE)
-			tt_score -= CHECKMATE - depth;
-		else if (tt_score < -CHECKMATE)
-			tt_score += CHECKMATE - depth;
-		ttable_store(board->hash, depth, tt_score, best_move, bound);
+		ttable_store(board->hash, depth, tt_score, best_move, tt_bound);
 	}
+
+	assert(best_score != -INF && best_score != INF);
 	return best_score;
 }
 
@@ -287,28 +299,121 @@ void search_reset(void) {
 	move_list_clear(&root_pv);
 }
 
-Move search_best_move(Board* board, SearchOptions* options, EngineConfig* cfg) {
+uint32_t search_calculate_time_budget(const SearchOptions* opts, Player p) {
+	log_trace("calculating time budget");
+	uint32_t remaining = p == PLAYER_W ? opts->wtime : opts->btime;
+	log_trace("remaining time: %u ms", remaining);
+	uint32_t increment = p == PLAYER_W ? opts->winc : opts->binc;
+	log_trace("increment: %u ms", increment);
+	uint32_t movestogo = opts->movestogo ? opts->movestogo : 40;  // use 40 as fallback
+
+	// no time control
+	if (!remaining && !increment) {
+		log_trace("no time control set");
+		return UINT32_MAX;
+	}
+
+	// add a fraction of the increment to the budget
+	uint32_t time = ((uint32_t) (remaining / movestogo)) + (increment * 0.75);
+
+	// ensure small reserve
+	if (time > 50)
+		time -= 50;
+
+	// ensure the search doesnt stop too early
+	if (time < 100)
+		time = 100;
+
+	log_trace("time budget: %u ms", time);
+	return time;
+}
+
+uint32_t timeval_to_ms(struct timeval tv) {
+	return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+
+uint32_t time_now(void) {
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return timeval_to_ms(tv);
+}
+
+void gstop_cond_eval(SearchOptions* options,
+					 uint32_t		nodes,
+					 uint32_t		time_start,
+					 uint32_t		time_limit) {
+	// NOTE: depth is evaluated by the function performing iterative deepening
+	if (stop) {
+		log_debug("search already stopped");
+		return;
+	}
+
+	if (options->nodes && nodes >= options->nodes) {
+		log_trace("node limit reached: set %u nodes %u", options->nodes, nodes);
+		stop = true;
+		return;
+	}
+
+	if (!options->infinite) {
+		uint32_t timenow = time_now();
+		if (timenow - time_start >= time_limit) {
+			log_trace("time limit reached: elapsed %u ms, time limit: %u ms",
+					  timenow - time_start,
+					  time_limit);
+			stop = true;
+			return;
+		}
+	}
+}
+
+Move search_best_move(Board* board, SearchOptions* opts, EngineConfig* cfg) {
+	assert(board != NULL);
+	assert(opts != NULL);
+	assert(cfg != NULL);
 	SearchContext ctx = {0};
 	stop			  = false;
+	ctx.time_start	  = time_now();
+	ctx.time_limit	  = search_calculate_time_budget(opts, board->side);
+	log_debug(
+		"search options - depth: %d, nodes: %d, movetime: %u, wtime: %u, btime: %u, winc: %u, "
+		"binc: %u, movestogo: %d, infinite: %d",
+		opts->depth,
+		opts->nodes,
+		opts->movetime,
+		opts->wtime,
+		opts->btime,
+		opts->winc,
+		opts->binc,
+		opts->movestogo,
+		opts->infinite);
 
+	// if depth isnt set, iterate until MAX_DEPTH-1 at most to avoid overflows
+	size_t max_depth = opts->depth != 0 ? opts->depth : MAX_DEPTH - 1;
 	// iterative deepening
-	for (size_t depth = 1; depth <= options->depth; depth++) {
-		if (stop) {
-			break;
-		}
-		// reset pvs for this search
+	for (size_t depth = 1; depth <= max_depth; depth++) {
 		memset(&pv_length, 0, sizeof(pv_length));
+		int score = search(depth, -INF, INF, 0, board, opts, &ctx, true);
 
-		int score = alpha_beta(&ctx, board, depth, -INF, INF, 0);
+		gstop_cond_eval(opts, ctx.nodes, ctx.time_start, ctx.time_limit);
+		if (stop)
+			break;
+
 		log_info("info depth %d score %d nodes %llu", depth, score, ctx.nodes);
-		move_list_clear(&root_pv);
-		for (size_t i = 0; i < pv_length[0]; ++i) {
-			move_list_push_back(&root_pv, pv_table[0][i]);
+		if (pv_length[0] >= move_list_size(&root_pv)) {
+			move_list_clear(&root_pv);
+			for (size_t i = 0; i < pv_length[0]; ++i) {
+				move_list_push_back(&root_pv, pv_table[0][i]);
+			}
+		} else {
+			for (size_t i = 0; i < pv_length[0]; ++i) {
+				Move* m = move_list_at(&root_pv, i);
+				*m		= pv_table[0][i];
+			}
 		}
 
 		log_info("PV:");
 		for (size_t i = 0; i < move_list_size(&root_pv); i++) {
-			log_info("%s", utils_move_description(*move_list_get(&root_pv, i)).str);
+			log_info("%s", utils_move_description(*move_list_at(&root_pv, i)).str);
 		}
 	}
 	assert(move_list_size(&root_pv) > 0);
@@ -318,4 +423,3 @@ Move search_best_move(Board* board, SearchOptions* options, EngineConfig* cfg) {
 void search_stop(void) {
 	stop = true;
 }
-
--- src/engine/search.h
+++ src/engine/search.h
@@ -1,7 +1,42 @@
 #ifndef SEARCH_H
 #define SEARCH_H
 
+#include "engine.h"
+#include "movelist.h"
 #include "types.h"
-Move search_best_move(Board* board, int depth);
+
+typedef struct {
+	MoveList* searchmoves;
+	uint32_t  depth;
+	uint32_t  nodes;
+	uint32_t  movetime;
+	uint32_t  wtime;
+	uint32_t  btime;
+	uint32_t  winc;
+	uint32_t  binc;
+	uint32_t  movestogo;
+	uint32_t  mate;	 // mate in x moves
+	bool	  ponder;
+	bool	  infinite;
+} SearchOptions;
+
+typedef struct {
+	uint32_t depth;
+	uint32_t seldepth;
+	// uint32_t multipv;
+	uint32_t score_cp;	// score in centipawns
+	uint64_t nodes;
+	// uint32_t nps;
+	// uint32_t hashfull;
+	// uint32_t tbhits;
+	uint32_t time;	// uint32_t can hold up to 1190 hours as ms
+
+	MoveList* pv;
+} SearchInfo;
+
+void search_init(void);
+void search_reset(void);
+Move search_best_move(Board* board, SearchOptions* options, EngineConfig* cfg);
+void search_stop(void);
 
 #endif	// SEARCH_H
